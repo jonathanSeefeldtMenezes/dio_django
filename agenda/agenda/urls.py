90714654079320996121ba201c11978cfe854e6d
jonathanSeefeldtMenezes <sitioinga@gmail.com>
2022-03-13 15:53:33 -0400

    Aula 03 - Criando uma página de listagem

--- agenda/agenda/urls.py
+++ agenda/agenda/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core import views
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('evento/<titulo_evento>', views.ObterLocalEvento)
+    path('evento/<titulo_evento>', views.obter_local_evento),
+    path('agenda/', views.listar_eventos),
+    path('', RedirectView.as_view(url='/agenda/'))
 ]
--- agenda/core/views.py
+++ agenda/core/views.py
@@ -3,9 +3,32 @@ from .models import Evento
 
 # Create your views here.
 
+#def index(request):
+#    return redirect('/agenda')
 
-def ObterLocalEvento(request, titulo_evento):
+
+def obter_local_evento(request, titulo_evento):
     _evento = Evento.objects.get(titulo=titulo_evento)
     resposta = f'Este é local do evento: {_evento.local}'
     print(len(resposta))
     return HttpResponse(resposta)
+
+
+def listar_eventos(request):
+    url_login = 'http://127.0.0.1:8000/admin/login/?next=/admin/'
+
+    try:
+        usuario = request.user
+        eventos = Evento.objects.filter(usuario=usuario)
+        dados = {'eventos': eventos, 'usuario': usuario}
+        return render(request, 'agenda.html', dados)
+
+    except BaseException as ex:
+        mensagem_erro = f'Mensagem de erro: {ex}.'
+
+        if 'AnonymousUser' in str(ex):
+            return HttpResponse(f'{mensagem_erro}<br>'
+                                f'<a href="{url_login}">Faça o Login</a>')
+        else:
+            return HttpResponse(mensagem_erro)
+
